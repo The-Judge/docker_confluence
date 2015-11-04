3f30c968950eece2bf40c7055485de77bfdf538f
Marc Richter <mr@in-telegence.net>
2015-11-04 09:57:13 +0100

    Added .gitignore and .dockerignore files for ignoring IDEs working dirs. Added Userscript hook for Timezone or other specific settings

--- startup.sh
+++ startup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+# If $USERSCRIPT exists it gets executed. Must exit with a clean error code (0)!
+USERSCRIPT=/user/init
+if [ -e ${USERSCRIPT} ]; then
+    chmod +x ${USERSCRIPT}
+    ${USERSCRIPT}
+fi
 pg_ctlcluster 9.4 main start -- -w
 sleep 3
 /usr/local/atlassian/confluence/bin/catalina.sh run
-
